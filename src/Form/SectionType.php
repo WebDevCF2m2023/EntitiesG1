7336609736eb608fff7d2820401ffefecb3a5089
WebDevCF2m <gitweb@cf2m.be>
2024-09-16 15:36:32 +0200

    cruds ok

--- src/Form/SectionType.php
+++ src/Form/SectionType.php
@@ -2,9 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Post;
 use App\Entity\Section;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,12 +14,7 @@ class SectionType extends AbstractType
         $builder
             ->add('sectionTitle')
             ->add('sectionDescription')
-            ->add('posts', EntityType::class, [
-                'class' => Post::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-                'expanded' => true,
-            ])
+
         ;
     }
 
